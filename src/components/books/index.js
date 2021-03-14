fb3668f4bdc1ec6a80fe7dd4f2563431f852c1a8
Angga Afrianto <anggaaurelius@gmail.com>
2021-03-14 14:23:34 +0700

    fetching data book

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,56 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
-import {logo} from '../../../assets/image';
+import {API} from '../../config/axios';
+import NumberFormat from 'react-number-format';
 
 const Books = () => {
+  const [loading, setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const getBooks = async () => {
+    try {
+      setLoading(true);
+      const books = await API.get('/books');
+      setBooks(books.data.data.books);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getBooks();
+  }, []);
+  const ok = 123;
   return (
     <View style={{flex: 1}}>
       <View style={{height: 150}}>
-        <Text
-          style={{
-            fontSize: 30,
-            margin: 20,
-            textAlign: 'center',
-          }}>
+        <Text style={{fontSize: 30, margin: 20, textAlign: 'center'}}>
           With us, you can shop online & help to save your high street at same
           time
         </Text>
       </View>
-      <View
-        style={{
-          flex: 1,
-          paddingHorizontal: 15,
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}>
-        <Image
-          source={logo}
-          style={{
-            width: 150,
-            height: 200,
-            borderRadius: 10,
-          }}
-        />
-        <View
-          style={{
-            flex: 1,
-            height: 200,
-            // backgroundColor: '#2ef',
-            marginLeft: 15,
-            marginBottom: 20,
-            marginTop: 10,
-          }}>
-          <Text style={{fontSize: 25}}>Title</Text>
-          <Text style={{fontSize: 18}}>Author</Text>
-          <Text style={{fontSize: 22, position: 'absolute', marginTop: 100}}>
-            Harga
-          </Text>
-          <View style={{flex: 1}} />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.text}>Add to cart</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      {loading ? (
+        <Text style={{fontSize: 30, textAlign: 'center'}}>Loading</Text>
+      ) : (
+        books.map((Books) => (
+          <View style={styles.card} key={Books.id}>
+            <Image
+              source={{uri: `${Books.thumbnail}`}}
+              style={{width: 150, height: 200, borderRadius: 10}}
+            />
+            <View style={styles.info}>
+              <Text
+                style={{fontSize: 25, fontWeight: 'bold'}}
+                numberOfLines={2}
+                ellipsizeMode="tail">
+                {Books.title}
+              </Text>
+              <Text style={{fontSize: 20, fontStyle: 'italic', color: 'grey'}}>
+                {Books.author}
+              </Text>
+              <NumberFormat
+                value={Books.price}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'Rp '}
+                renderText={(price) => (
+                  <Text style={styles.price}>{price}</Text>
+                )}
+              />
+              <View style={{flex: 1}} />
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.text}>Add to cart</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))
+      )}
     </View>
   );
 };
@@ -63,12 +76,35 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderWidth: 3,
     paddingVertical: 6,
+    position: 'absolute',
+    bottom: -5,
   },
   text: {
     fontSize: 18,
     color: '#fff',
     fontWeight: 'bold',
     alignSelf: 'center',
+  },
+  card: {
+    flex: 1,
+    paddingHorizontal: 15,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  info: {
+    flex: 1,
+    height: 200,
+    marginLeft: 15,
+    marginBottom: 20,
+    marginTop: 10,
+  },
+  price: {
+    fontSize: 22,
+    position: 'absolute',
+    marginTop: 100,
+    color: 'green',
+    fontWeight: 'bold',
   },
 });
 export default Books;
